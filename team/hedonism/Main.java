79d3332c71e6f21f738510c27dad2b6d3ee9d7ed
taganame <71151775+taganame@users.noreply.github.com>
2021-12-09 19:45:46 +0300

    Add files via upload

---
+++ team/hedonism/Main.java
@@ -0,0 +1,8 @@
+package team.hedonism;
+
+public class Main {
+    public static boolean DEBUG = true;
+    public static int PHILOSOPHERS_COUNT = 5;
+    public static int MAX_WAIT_MS = 1;
+    public static int DINNER_DURATION_IN_MS = 1000;
+}
